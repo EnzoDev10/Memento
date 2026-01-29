705eb6d3259cd82314a2df05b758f7448a1794b6
EnzoDev10 <Enzodev4@gmail.com>
2026-01-29 16:56:02 -0300

    Add animation for Author field in QuoteSelector.

--- src/features/Form/components/QuoteSelector/QuoteSelector.tsx
+++ src/features/Form/components/QuoteSelector/QuoteSelector.tsx
@@ -1,16 +1,48 @@
 import { StyledInput, StyledWrapper } from "@/components/Reusables";
 import { DateContext } from "@/global/DateContext";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import styled, { keyframes } from "styled-components";
 
 interface QuotesProps {
     setQuote: (value: string) => void;
     setAuthor: (value: string) => void;
 }
 
+const animationTest = keyframes`
+    0% {
+        display:none;
+        top: -5px;
+    }
+
+    1% {
+        display: block;
+        opacity: 0;
+    }
+
+    100% {
+        top: 0;
+        opacity: 1;
+    }
+
+`;
+
+const AnimatedWrapper = styled(StyledWrapper)`
+    animation: ${animationTest} 500ms ease-in-out forwards;
+    position: relative;
+`;
+
 export const QuoteSelector = ({ setQuote, setAuthor }: QuotesProps) => {
     const { setChange, quote, author } = useContext(DateContext);
 
+    const [showAuthorInput, setShowAuthorInput] = useState(false);
+
     const handleQuoteChange = (value: string) => {
+        if (value != "") {
+            setShowAuthorInput(true);
+        } else {
+            setShowAuthorInput(false);
+        }
+
         setQuote(value);
         setChange(false);
     };
@@ -33,17 +65,20 @@ export const QuoteSelector = ({ setQuote, setAuthor }: QuotesProps) => {
                     />
                 </StyledInput>
             </StyledWrapper>
-            <StyledWrapper>
-                <label>Author</label>
-                <StyledInput>
-                    <input
-                        type='text'
-                        placeholder='Write the author here...'
-                        onChange={(e) => handleAuthorChange(e.target.value)}
-                        value={author}
-                    />
-                </StyledInput>
-            </StyledWrapper>
+
+            {showAuthorInput && (
+                <AnimatedWrapper>
+                    <label>Author</label>
+                    <StyledInput>
+                        <input
+                            type='text'
+                            placeholder='Write the author here...'
+                            onChange={(e) => handleAuthorChange(e.target.value)}
+                            value={author}
+                        />
+                    </StyledInput>
+                </AnimatedWrapper>
+            )}
         </>
     );
 };
